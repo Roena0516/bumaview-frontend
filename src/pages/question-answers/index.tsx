1bc729c05d5f03e9e568d83c70f2f7f2cc9c02fb
Roena0516 <tkf2001.yc@gmail.com>
2025-09-17 14:48:07 +0900

    목데이터 수정

--- src/pages/question-answers/index.tsx
+++ src/pages/question-answers/index.tsx
@@ -1,6 +1,6 @@
-import { useNavigation } from '../../shared/context/NavigationContext';
-import { BackIcon } from '../../shared/components/BackIcon';
-import * as styles from './style';
+import { useNavigation } from "../../shared/context/NavigationContext";
+import { BackIcon } from "../../shared/components/BackIcon";
+import * as styles from "./style";
 
 interface Answer {
   id: number;
@@ -20,59 +20,63 @@ interface QuestionAnswersPageProps {
 const mockAnswers: Answer[] = [
   {
     id: 1,
-    content: "저는 대단한 개발자입니다. 저를 뽑지 않으면 큰 후회를 하게 될 것입니다.",
+    content:
+      "저는 대단한 개발자입니다. 저를 뽑지 않으면 큰 후회를 하게 될 것입니다.",
     userName: "박동현",
     time: "1:23",
-    score: 0.21
+    score: 0.21,
   },
   {
     id: 2,
     content: "넹",
     userName: "박소은",
     time: "0:03",
-    score: 1.23
+    score: 1.23,
   },
   {
     id: 3,
-    content: "저는 프론트를 중심적으로 공부하고 개발하는 개발자입니다. 저를 뽑아주신다면 정말 열심히 하겠",
+    content:
+      "저는 프론트를 중심적으로 공부하고 개발하는 개발자입니다. 저를 뽑아주신다면 정말 열심히 하겠습니다.",
     userName: "류승찬",
     time: "1:54",
-    score: 2.43
+    score: 2.43,
   },
   {
     id: 4,
-    content: "저는 백엔드를 중심으로 개발하지만, 다양한 분야를 커버할 수 있습니다. 알지 못하는 분야라도 열",
+    content:
+      "저는 백엔드를 중심으로 개발하지만, 다양한 분야를 커버할 수 있습니다. 알지 못하는 분야라도 열심히 공부하여 빠르게 적응할 자신이 있습니다.",
     userName: "박동현",
     time: "5:45",
-    score: 3.85
+    score: 3.85,
   },
   {
     id: 5,
-    content: "저는 백엔드를 좋아하는 개발자입니다. 하지만 백엔드 외에도 프론트엔드, 디자인과 같이 다른 분야도",
+    content:
+      "저는 백엔드를 좋아하는 개발자입니다. 하지만 백엔드 외에도 프론트엔드, 디자인과 같이 다른 분야도 관심이 많습니다. 다양한 분야를 경험해보고 싶습니다.",
     userName: "김유찬",
     time: "2:43",
-    score: 4.54
-  }
+    score: 4.54,
+  },
 ];
 
 export const QuestionAnswersPage = ({
   questionTitle = "간단한 자기소개 부탁드립니다.",
   company = "마이다스IT",
   field = "백엔드",
-  year = "2023"
+  year = "2023",
 }: QuestionAnswersPageProps) => {
   const { navigateToPage } = useNavigation();
 
   const handleLoginClick = () => {
-    navigateToPage('login');
+    navigateToPage("login");
   };
 
   const handleSignupClick = () => {
-    navigateToPage('signup');
+    navigateToPage("signup");
   };
 
   const handleBackClick = () => {
-    navigateToPage('main');
+    navigateToPage("main");
   };
 
   return (
@@ -98,9 +102,7 @@ export const QuestionAnswersPage = ({
       {/* 질문 섹션 */}
       <div className={styles.questionSection}>
         <div className={styles.questionInfo}>
-          <div className={styles.questionTitle}>
-            {questionTitle}
-          </div>
+          <div className={styles.questionTitle}>{questionTitle}</div>
           <div className={styles.questionDetail}>
             {company} / {field} / {year}
           </div>
@@ -136,9 +138,7 @@ export const QuestionAnswersPage = ({
                 className={styles.answerItem}
                 style={{ backgroundColor: styles.getScoreColor(answer.score) }}
               >
-                <div className={styles.answerContent}>
-                  {answer.content}
-                </div>
+                <div className={styles.answerContent}>{answer.content}</div>
                 <div className={styles.answerDetails}>
                   <div className={styles.answerName}>{answer.userName}</div>
                   <div className={styles.answerTime}>{answer.time}</div>
